1838c09004a349ec491968c0b7243d38ac64a4b4
unknown <sujeong.leesujung0527@gmail.com>
2020-09-01 15:34:26 +0900

    home commit

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
            public static void main(String[] args){
-                       system.out.println("Hello, World");  
+                       system.out.println("Hello, World");
+
+                  // home. lol  
            }   
 }
